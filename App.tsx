4b01bd1aaf10dbb9537c0dbbe8675b972fbfac68
tomoya <alligatorfree12@gmail.com>
2025-01-29 07:28:06 +0900

    flexbox

--- App.tsx
+++ App.tsx
@@ -1,23 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, StatusBar } from 'react-native';
+import styles from './styles';
+import Box from './Box';
+
+const boxes = new Array(10).fill(null).map((v, i) => i + 1);
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>
-        Open up App.tsx to start working on your app!
-      </Text>
-      <StatusBar style="auto" />
+      <StatusBar hidden={false} />
+      {boxes.map((i) => (
+        <Box key={i}>#{i}</Box>
+      ))}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: { fontWeight: "bold" },
-});
